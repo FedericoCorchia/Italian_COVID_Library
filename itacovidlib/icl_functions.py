6f1d53d7a6525bf38b975fca9e2adc2ffd3f6917
FedericoCorchia <fedecorchia@gmail.com>
2021-09-16 19:54:56 +0200

    edited icl_functions.py for connection issues handling

--- itacovidlib/icl_functions.py
+++ itacovidlib/icl_functions.py
@@ -4,8 +4,14 @@ import io
 
 def get(url):
     """Docstring to be added when function is completed."""
-    downloaded_content = requests.get(url).content
-    dataframe = pd.read_csv(io.StringIO(downloaded_content.decode('utf-8')))
-    return dataframe
+    try:
+        downloaded_content = requests.get(url).content
+        dataframe = pd.read_csv(io.StringIO(downloaded_content.decode('utf-8')))
+        return dataframe
+    except requests.exceptions.MissingSchema:
+        print("Unvalid URL provided")
+    except requests.exceptions.ConnectionError:
+        print("Failure to connect to URL")
+
         
 
