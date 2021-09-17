2b480a4ab1089e12f119a085fc1b60655b99b29b
FedericoCorchia <fedecorchia@gmail.com>
2021-09-17 12:14:41 +0200

    code quality improvements

--- itacovidlib/icl_functions.py
+++ itacovidlib/icl_functions.py
@@ -2,40 +2,73 @@ import pandas as pd
 import requests
 import io
 
-class COVIDDataSource:
-    def __init__(self, url: str):
-        self.url = url
+
+def get_vaccine_ages_sum_latest():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/anagrafica-vaccini-summary-latest.csv")
+
+def get_vaccine_deliveries_latest():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/consegne-vaccini-latest.csv")
+
+def get_eligible():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/platea.csv")
+
+def get_admin_sites_latest():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-latest.csv")
+
+def get_admin_sites_kinds():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-tipologia.csv")
+
+def get_vaccine_admin_latest():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv")
+
+def get_vaccine_admin_sum_latest():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv")
+
+def get_vaccine_sum_latest():
+    return get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.csv")
+
+def get_national_trend():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv")
+
+def get_national_trend_latest():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-latest.csv")
+
+def get_equip_contracts():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-contratti-dpc-forniture/dpc-covid19-dati-contratti-dpc-forniture.csv")
+
+def get_equip_contracts_payments():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-contratti-dpc-forniture/dpc-covid19-dati-pagamenti-contratti-dpc-forniture.csv")
+
+def get_province_cases():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv")
+
+def get_province_cases_latest():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-latest.csv")
+
+def get_region_cases_latest():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv")
+
+def get_region_cases():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv")
+
+def get_over_80():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-over80.csv")
+
+def get_istat_region_data():
+    return get("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv")
 
 
-vaccine_age_ranges_summary_latest = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/anagrafica-vaccini-summary-latest.csv")
-vaccine_deliveries_latest = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/consegne-vaccini-latest.csv")
-eligible = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/platea.csv")
-administration_sites_latest = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-latest.csv")
-administration_sites_kinds = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-tipologia.csv")
-vaccine_administration_latest = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv")
-vaccine_administration_summary_latest = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv")
-vaccine_summary_latest = COVIDDataSource("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.csv")
-national_trend = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv")
-national_trend_latest = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-latest.csv")
-equipment_contracts = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-contratti-dpc-forniture/dpc-covid19-dati-contratti-dpc-forniture.csv")
-equipment_contracts_payments = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-contratti-dpc-forniture/dpc-covid19-dati-pagamenti-contratti-dpc-forniture.csv")
-province_cases = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv")
-province_cases_latest = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-latest.csv")
-region_cases_latest = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-latest.csv")
-region_cases = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv")
-over80 = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-over80.csv")
-istat_region_data = COVIDDataSource("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv")
 
-def get(covid_data_source):
+def get(url):
     """Docstring to be added when function is completed."""
     try:
-        downloaded_content = requests.get(covid_data_source.url).content
+        downloaded_content = requests.get(url).content
         dataframe = pd.read_csv(io.StringIO(downloaded_content.decode('utf-8')))
         return dataframe
     except requests.exceptions.MissingSchema:
-        print("Unvalid URL provided")
+        print("ERROR Unvalid URL provided")
     except requests.exceptions.ConnectionError:
-        print("Failure to connect to URL")
+        print("ERROR Connection failure")
 
 
 
--- tests/test_icl_functions.py
+++ tests/test_icl_functions.py
@@ -5,15 +5,13 @@ working_dir = os.path.basename(os.path.normpath(os.getcwd()))
 
 sys.path.append("../itacovidlib")
 if working_dir == "covid_lib":
-    from itacovidlib.icl_functions import get, COVIDDataSource
+    from itacovidlib.icl_functions import get
 elif working_dir == "tests":
     sys.path.append("../itacovidlib")
-    from icl_functions import get, COVIDDataSource
+    from icl_functions import get
 
 def test_wrong_url():
-    data_source_with_nonsense_url = COVIDDataSource("nonsense_url")
-    test_dataframe = get(data_source_with_nonsense_url)
+    test_dataframe = get("wrong_url")
 
 def test_wrong_url_2():
-    data_source_with_wrong_url = COVIDDataSource("http://fakeurl")
-    test_dataframe = get(data_source_with_wrong_url)
+    test_dataframe = get("http://fakeurl")
