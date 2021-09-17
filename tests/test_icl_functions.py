843f52b55a57f189d37225712aa073008cc643bd
FedericoCorchia <fedecorchia@gmail.com>
2021-09-17 11:31:25 +0200

    updated test_icl_functions.py: now tests handle the new argument of get()

--- tests/test_icl_functions.py
+++ tests/test_icl_functions.py
@@ -5,13 +5,15 @@ working_dir = os.path.basename(os.path.normpath(os.getcwd()))
 
 sys.path.append("../itacovidlib")
 if working_dir == "covid_lib":
-    from itacovidlib.icl_functions import get
+    from itacovidlib.icl_functions import get, COVIDDataSource
 elif working_dir == "tests":
     sys.path.append("../itacovidlib")
-    from icl_functions import get
+    from icl_functions import get, COVIDDataSource
 
 def test_wrong_url():
-    test_dataframe = get("wrong_url")
+    data_source_with_nonsense_url = COVIDDataSource("nonsense_url")
+    test_dataframe = get(data_source_with_nonsense_url)
 
 def test_wrong_url_2():
-    test_dataframe = get("http://fakeurl")
+    data_source_with_wrong_url = COVIDDataSource("http://fakeurl")
+    test_dataframe = get(data_source_with_wrong_url)
