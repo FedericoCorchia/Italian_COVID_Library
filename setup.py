b4780b66f9bb472acbe635087b96a8c0969d6fd4
FedericoCorchia <fedecorchia@gmail.com>
2021-09-18 11:20:54 +0200

    edited setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import find_packages, setup
+import os
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
 
 setup(
     name='itacovidlib',
@@ -6,5 +11,9 @@ setup(
     version='0.1.0'
     description='Python library for COVID-19 data analysis referred to Italy',
     author='Federico Corchia'
-    license=''
+    license='',
+    keywords='COVID-19 cases vaccines Italy'
+    url='https://github.com/FedericoCorchia/Italian_COVID_Library'
+    long_description=read('README.md')
+    install_requires=['pandas', 'requests'],
 )
