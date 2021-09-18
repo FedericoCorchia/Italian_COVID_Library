80035a76d929297e1e5cf86c0bd9e3868716b3d3
FedericoCorchia <fedecorchia@gmail.com>
2021-09-18 10:58:17 +0200

    edited setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='itacovidlib',
+    packages=find_packages(),
+    version='0.1.0'
+    description='Python library for COVID-19 data analysis referred to Italy',
+    author='Federico Corchia'
+    license=''
+)
