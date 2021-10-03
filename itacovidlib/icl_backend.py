dd634a7b339120af8c0100a742ebddfbf7cd46a3
FedericoCorchia <fedecorchia@gmail.com>
2021-10-03 16:32:35 +0200

    improved behaviour in case of no Internet connection. Documentation also updated accordingly

--- itacovidlib/icl_backend.py
+++ itacovidlib/icl_backend.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import requests
 import io
+import sys
+
+# errors are shown as clearly coming from Italian COVID Library to distinguish them from the ones raised by other libraries.
+class ItaCovidLibConnectionError(requests.exceptions.ConnectionError): pass
 
 def get(url):
     """Returns a dataframe from the .csv file at which the URL provided as a parameter points, properly parsing it. Meant to be invoked by get_<resource_name>() functions.
@@ -12,7 +16,8 @@ def get(url):
     
     Raises
     ------
-    None
+    ItaCovidLibConnectionError
+        Connection error coming from Italian COVID Library, making it clear to the user it comes from this library and not other ones. This error is raised when the library fails to get data from the Internet, e.g. for lack of Internet connection.
     
     Returns
     -------
@@ -27,9 +32,7 @@ def get(url):
         downloaded_content = requests.get(url).content
         dataframe = pd.read_csv(io.StringIO(downloaded_content.decode('utf-8')))
         return dataframe
-    # handles the case when string url has not the syntax of a real URL
-    except requests.exceptions.MissingSchema:
-        print("ERROR Unvalid URL provided")
-    # handles connection issues (both lack of Internet connection or URL leading to no website)
+    # error reraising makes it clear to the user the error was actually raised by Italian COVID Library and not other libraries.
     except requests.exceptions.ConnectionError:
-        print("ERROR Connection failure")
+        raise ItaCovidLibConnectionError("connection failure. Most probable cause is lack of Internet connection.") from None
+
