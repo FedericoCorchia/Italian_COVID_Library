eff08cf5c840c29cf64cd53f69e5f16af5297f96
FedericoCorchia <fedecorchia@gmail.com>
2021-10-03 17:19:14 +0200

    edited test_icl_functions.py with new test

--- tests/test_icl_functions.py
+++ tests/test_icl_functions.py
@@ -1,18 +1,18 @@
 import sys
 
 sys.path.append("../itacovidlib")
-import itacovidlib.icl_backend#, itacovidlib.icl_functions
+import itacovidlib.icl_backend as icl_b, itacovidlib.icl_functions as icl
 
 def test_no_Internet_connection():
     # to be performed under no Internet connection
     try:
-        itacovidlib.icl_backend.get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/anagrafica-vaccini-summary-latest.csv")
-    except itacovidlib.icl_backend.ItaCovidLibConnectionError:
+        icl_b.get("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/anagrafica-vaccini-summary-latest.csv")
+    except icl_b.ItaCovidLibConnectionError:
         print("No Internet connection exception handled successfully.")
 
-#def test_no_Internet_connection_2():
+def test_no_Internet_connection_2():
     # to be performed under no Internet connection. Same test also applies logically to all other get_<resource_name>() functions, since they work in exactly the same way.
-#    try:
-#        itacovidlib.icl_functions.get_vaccine_ages()
-#    except itacovidlib.icl_backend.ItaCovidLibConnectionError:
-#        print("No Internet connection exception handled successfully.")
+    try:
+        icl.get_vaccine_ages()
+    except icl_b.ItaCovidLibConnectionError:
+        print("No Internet connection exception handled successfully.")
