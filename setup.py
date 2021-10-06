dd281e4b3aea0047b94043fface9fe012ca20bc3
FedericoCorchia <fedecorchia@gmail.com>
2021-10-06 11:15:35 +0200

    updated requirements.txt, setup.py and itacovidlib_env.yml with current library requirements

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     name='itacovidlib',
     packages=find_packages(),
     version='0.1.0'
-    description='Python library for COVID-19 data analysis referred to Italy',
+    description='Python library for COVID-19 infections and vaccinations in Italy data retrieval and analysis',
     author='Federico Corchia'
     license='',
-    keywords='COVID-19 cases vaccines Italy'
+    keywords='COVID-19 infection vaccination Italy'
     url='https://github.com/FedericoCorchia/Italian_COVID_Library'
     long_description=read('README.md')
-    install_requires=['pandas', 'requests'],
+    install_requires=['numpy', 'pandas', 'geopandas', 'requests'],
 )
