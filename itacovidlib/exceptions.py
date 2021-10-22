3e571fb72df0718ba77dfc65494f350967a9a72d
FedericoCorchia <fedecorchia@gmail.com>
2021-10-23 01:25:23 +0200

    updated documentation in Tutorial.ipynb and exceptions.py

--- itacovidlib/exceptions.py
+++ itacovidlib/exceptions.py
@@ -5,7 +5,7 @@ class ItaCovidLibConnectionError(requests.exceptions.ConnectionError):
     """Raised when a connection error occurs (e.g. because of lack of Internet connection) in an Italian COVID Library function"""
     pass
 class ItaCovidLibArgumentError(Exception):
-    """Raised when improper arguments (of the requested type, but not among the acceptable ones) are passed to an Italian COVID Library function"""
+    """Raised when improper arguments are passed to an Italian COVID Library function"""
     pass
 class ItaCovidLibKeyError(KeyError):
     """Raised when missing of a key with a determinate name in a DataFrame prevents an Italian COVID Library function from working"""
