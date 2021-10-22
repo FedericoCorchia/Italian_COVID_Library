5407a33b021b2ad938c3db464501cb70302f89bb
FedericoCorchia <79851636+FedericoCorchia@users.noreply.github.com>
2021-10-23 01:47:43 +0200

    updated download_url in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='MIT',
     keywords='COVID-19 infection vaccination Italy',
     url='https://github.com/FedericoCorchia/Italian_COVID_Library',
-    download_url='https://github.com/FedericoCorchia/itacovidlib/archive/refs/tags/v0.1.0-alpha.tar.gz',
+    download_url='https://github.com/FedericoCorchia/itacovidlib/archive/refs/tags/v0.1.1-alpha.tar.gz',
     long_description=read('README.md'),
     install_requires=['numpy', 'pandas', 'geopandas', 'requests', 'epyestim'],
     classifiers=[
