8ea10ebcd07c66d7d762730847ba8a13bd5a25da
FedericoCorchia <79851636+FedericoCorchia@users.noreply.github.com>
2021-10-23 14:52:45 +0200

    added more precise specification in install_requires and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -17,9 +17,10 @@ setup(
     url='https://github.com/FedericoCorchia/Italian_COVID_Library',
     download_url='https://github.com/FedericoCorchia/itacovidlib/archive/refs/tags/v0.1.1-alpha.tar.gz',
     long_description=read('README.md'),
-    install_requires=['numpy', 'pandas', 'geopandas', 'requests', 'epyestim'],
+    install_requires=['numpy', 'pandas>=1.2.0', 'geopandas>=0.7.0', 'requests', 'epyestim'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Utilities',
-        ],
+        'License :: OSI Approved :: MIT License',
+    ],
 )
